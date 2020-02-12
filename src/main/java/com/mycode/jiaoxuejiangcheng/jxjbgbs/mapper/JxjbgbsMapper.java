4ce245c994575cbdf01db3bc1b6a070c22715375
keXiangwei <15313725815@189.cn>
2020-02-12 16:14:28 +0800

    教学奖惩-教学基本功比赛

---
+++ src/main/java/com/mycode/jiaoxuejiangcheng/jxjbgbs/mapper/JxjbgbsMapper.java
@@ -0,0 +1,25 @@
+package com.mycode.jiaoxuejiangcheng.jxjbgbs.mapper;
+
+import com.mycode.jiaoxuejiangcheng.jxjbgbs.domian.Jxjbgbs;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 教学奖惩-教学基本功比赛
+ */
+@Mapper
+public interface JxjbgbsMapper {
+
+    List<Jxjbgbs> getPageList(Jxjbgbs obj);
+
+    int getNotShenHeNum(@Param("shenHeUserId") Integer shenHeUserId);
+
+    boolean insert(Jxjbgbs obj);
+
+    boolean update(Jxjbgbs obj);
+
+    boolean delete(@Param("objCode") String objCode);
+
+}
---
+++ src/main/java/com/mycode/jiaoxuejiangcheng/jxjbgbs/service/JxjbgbsService.java
@@ -0,0 +1,25 @@
+package com.mycode.jiaoxuejiangcheng.jxjbgbs.service;
+
+import com.mycode.common.shenhe.domain.ShenHeItem;
+import com.mycode.jiaoxuejiangcheng.jxjbgbs.domian.Jxjbgbs;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 教学奖惩-教学基本功比赛
+ */
+public interface JxjbgbsService {
+
+    Map<String, Object> getPageList(Jxjbgbs obj);
+
+    boolean insert(Jxjbgbs obj);
+
+    boolean update(Jxjbgbs obj);
+
+    boolean delete(String objCode);
+
+    boolean toSubimt(String activeShenheCode, List<Jxjbgbs> objList);
+
+    boolean toShenhe(ShenHeItem item, List<Jxjbgbs> objList);
+}
