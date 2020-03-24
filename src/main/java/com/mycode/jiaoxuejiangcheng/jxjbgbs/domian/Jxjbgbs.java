c84a2e404108f9f470670efcf6ebe85ebcac0a93
keXiangwei <15313725815@189.cn>
2020-03-24 14:08:14 +0800

    【教学基本功比赛】

--- src/main/java/com/mycode/jiaoxuejiangcheng/jxjbgbs/domian/Jxjbgbs.java
+++ src/main/java/com/mycode/jiaoxuejiangcheng/jxjbgbs/domian/Jxjbgbs.java
@@ -19,11 +19,11 @@ public class Jxjbgbs {
     @JsonIgnore
     private Integer pageIndex=1
             ,pageSize=10;
-
     //
     private Integer shenHeUserId //获取审核列表参数
             ,userId; //获取信息列表|查询参数
-    private String userName;
+    private String userName
+            ,userUnit;
 
     //逻辑字段
     private String isSubmit //提交状态【已提交 | 未提交】
@@ -36,10 +36,9 @@ public class Jxjbgbs {
     private String code //业务数据编号
             ,level1 //级别，一级下拉框【国家级|北京市级|校级】
             ,level2 //奖项，二级下拉框[一等奖|二等奖|三等奖|其他]
-            ,grantUnit; //证书授予机构
-    @JsonFormat(pattern="yyyy-MM-dd", locale="zh", timezone="GMT+8")
-    @DateTimeFormat(pattern="yyyy-MM-dd")
-    private Date awardDate; //获奖日期
+            ,grantUnit //证书授予机构
+            ,datetimeYear,datetimeYearStart,datetimeYearEnd; //获奖日期(年)
+
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", locale="zh", timezone="GMT+8")
     @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date createDate; //业务数据录入时间
--- src/main/java/com/mycode/jiaoxuejiangcheng/jxjbgbs/service/JxjbgbsServiceImpl.java
+++ src/main/java/com/mycode/jiaoxuejiangcheng/jxjbgbs/service/JxjbgbsServiceImpl.java
@@ -9,9 +9,9 @@ import com.mycode.common.shenhe.domain.ShenHeNode;
 import com.mycode.common.shenhe.mapper.ShenHeMapper;
 import com.mycode.jiaoxuejiangcheng.jxjbgbs.domian.Jxjbgbs;
 import com.mycode.jiaoxuejiangcheng.jxjbgbs.mapper.JxjbgbsMapper;
+import com.mycode.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
 import java.util.List;
@@ -33,12 +33,12 @@ public class JxjbgbsServiceImpl implements JxjbgbsService {
     @Override
     public Map<String, Object> getPageList(Jxjbgbs obj) {
         Map<String, Object> resultMap = new HashMap<>();
-        Page<Object> pageInfo = PageHelper.startPage(obj.getPageIndex(), obj.getPageSize());
-        List<Jxjbgbs> pageList = jxjbgbsMapper.getPageList(obj);
-        if(!StringUtils.isEmpty(obj.getShenHeUserId())){
+        if(StringUtils.isNotEmpty(obj.getShenHeUserId())){
             int unShenHeNum = jxjbgbsMapper.getNotShenHeNum(obj.getShenHeUserId());//获取未审核数
             resultMap.put("unShenHeNum", unShenHeNum);
         }
+        Page<Object> pageInfo = PageHelper.startPage(obj.getPageIndex(), obj.getPageSize());
+        List<Jxjbgbs> pageList = jxjbgbsMapper.getPageList(obj);
         resultMap.put("totalNum",pageInfo.getTotal());
         resultMap.put("pageList", pageList);
         return resultMap;
