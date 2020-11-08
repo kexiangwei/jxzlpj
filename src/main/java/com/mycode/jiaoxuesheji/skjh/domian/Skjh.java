c80646f2837150d41745194d7d99b48963b871d2
keXiangwei <15313725815@189.cn>
2020-11-08 19:47:27 +0800

    20201108

--- src/main/java/com/mycode/jiaoxuesheji/skjh/domian/Skjh.java
+++ src/main/java/com/mycode/jiaoxuesheji/skjh/domian/Skjh.java
@@ -1,13 +1,8 @@
 package com.mycode.jiaoxuesheji.skjh.domian;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.mycode.commonset.shenheSet.domain.ShenHeObj;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.util.Date;
 
 /**
  * 教学设计-授课计划
@@ -15,21 +10,6 @@ import java.util.Date;
 @Getter
 @Setter
 public class Skjh extends ShenHeObj {
-
-    //分页参数
-    @JsonIgnore
-    private Integer pageIndex=1
-            ,pageSize=10;
-    //
-    private String shenHeUserId //获取审核列表参数
-            ,userId;
-    private String userName;
-    //逻辑字段
-    private String isSubmit //提交状态【已提交 | 未提交】
-            ,shenheCode;//审核编号
-    private Integer batchNum; //提交批次
-    private String status //数据状态：【审核中 | 通过 | 退回】
-            ,shenheStatus; //审核状态：【已审核 | 待审核 | 退回】
 
     //业务字段
     private String code //业务编号
@@ -44,11 +24,5 @@ public class Skjh extends ShenHeObj {
             ,stuScore //学分
             ,mainTeacher //主讲教师
             ,teachClass; //授课班级
-    //
-    /*private List<SkjhItem> itemList;*/
-    //
-    @JsonFormat(pattern="yyyy-MM-dd", locale="zh", timezone="GMT+8")
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private Date createDate; //业务数据录入时间
 
 }
