51475249cad6e3e659e2b79f553beff3b08650fa
keXiangwei <15313725815@189.cn>
2021-06-04 13:56:20 +0800

    520

--- src/test/java/com/mycode/ApplicationTests.java
+++ src/test/java/com/mycode/ApplicationTests.java
@@ -5,9 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
@@ -17,27 +14,13 @@ public class ApplicationTests {
 
         for (int i = 0; i < 10; i++) {
             if(i == 3){
-                continue; // 0 1 2 4 5 6 7 8 9 test         跳过本次循环
-//                break; //0 1 2 test       跳出循环
+                continue; // 0 1 2 4 5 6 7 8 9 后置语句         跳过本次循环
+//                break; //0 1 2 后置语句       跳出当前循环
 //                return; //0 1 2       方法结束
             }
             System.out.println(i);
         }
-        System.out.println("test");
-    }
-
-    @Test
-    public void test() {
-        Map<String,Object> map = new ConcurrentHashMap<>();
-        map.put("a",1);
-        map.put("b",2);
-        map.put("c",3);
-        map.forEach((k,v) -> {
-            if("b".equals(k)){
-                map.remove(k);
-            }
-        });
-        map.forEach((k,v) -> System.out.println(k));
+        System.out.println("后置语句");
     }
 
 }
